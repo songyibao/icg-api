9a52de04766e9753760d52d96846c812fb1911bd
SongYibao <songybmail@foxmail.com>
2025-06-06 18:41:00 +0800

    引入cos对象存储

---
+++ src/main/java/com/yb/icgapi/controller/FileController.java
@@ -0,0 +1,57 @@
+package com.yb.icgapi.controller;
+
+import com.yb.icgapi.annotation.AuthCheck;
+import com.yb.icgapi.common.BaseResponse;
+import com.yb.icgapi.common.ResultUtils;
+import com.yb.icgapi.constant.UserConstant;
+import com.yb.icgapi.exception.BusinessException;
+import com.yb.icgapi.exception.ErrorCode;
+import com.yb.icgapi.manager.CosManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+
+@Slf4j
+public class FileController {
+    @Resource
+    private CosManager cosManager;
+
+    /**
+     * 测试文件上传
+     *
+     * @param multipartFile
+     * @return
+     */
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @PostMapping("/test/upload")
+    public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
+        // 文件目录
+        String filename = multipartFile.getOriginalFilename();
+        String filepath = String.format("/test/%s", filename);
+        File file = null;
+        try {
+            // 上传文件
+            file = File.createTempFile(filepath, null);
+            multipartFile.transferTo(file);
+            cosManager.putObject(filepath, file);
+            // 返回可访问地址
+            return ResultUtils.success(filepath);
+        } catch (Exception e) {
+            log.error("file upload error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SERVER_ERROR, "上传失败");
+        } finally {
+            if (file != null) {
+                // 删除临时文件
+                boolean delete = file.delete();
+                if (!delete) {
+                    log.error("file delete error, filepath = {}", filepath);
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/yb/icgapi/manager/CosManager.java
@@ -0,0 +1,33 @@
+package com.yb.icgapi.manager;
+
+import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.PutObjectRequest;
+import com.qcloud.cos.model.PutObjectResult;
+import com.yb.icgapi.config.CosClientConfig;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.File;
+
+@Component
+public class CosManager {  
+  
+    @Resource
+    private CosClientConfig cosClientConfig;
+  
+    @Resource  
+    private COSClient cosClient;
+
+    /**
+     * 上传对象
+     *
+     * @param key  唯一键
+     * @param file 文件
+     */
+    public PutObjectResult putObject(String key, File file) {
+        PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key,
+                file);
+        return cosClient.putObject(putObjectRequest);
+    }
+
+}
