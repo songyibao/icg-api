f7c30af9ba1fe3fd27c901e8b462148397a1f2af
SongYibao <songybmail@foxmail.com>
2025-06-06 12:06:59 +0800

    add: 分页查询用户

--- src/main/java/com/yb/icgapi/controller/UserController.java
+++ src/main/java/com/yb/icgapi/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.yb.icgapi.controller;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yb.icgapi.annotation.AuthCheck;
 import com.yb.icgapi.common.BaseResponse;
 import com.yb.icgapi.common.DeleteRequest;
@@ -8,17 +9,17 @@ import com.yb.icgapi.common.ResultUtils;
 import com.yb.icgapi.constant.UserConstant;
 import com.yb.icgapi.exception.ErrorCode;
 import com.yb.icgapi.exception.ThrowUtils;
-import com.yb.icgapi.model.dto.user.UserAddRequest;
-import com.yb.icgapi.model.dto.user.UserLoginRequest;
-import com.yb.icgapi.model.dto.user.UserRegisterRequest;
+import com.yb.icgapi.model.dto.user.*;
 import com.yb.icgapi.model.vo.LoginUserVO;
 import com.yb.icgapi.model.entity.User;
+import com.yb.icgapi.model.vo.UserVO;
 import com.yb.icgapi.service.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -98,4 +99,30 @@ public class UserController {
         ThrowUtils.ThrowIf(!res, ErrorCode.SERVER_ERROR, "删除用户失败");
         return ResultUtils.success(true);
     }
+
+    @PostMapping("/update")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> updateUser(@RequestBody UserUpdateRequest userUpdateRequest) {
+        ThrowUtils.ThrowIf(userUpdateRequest == null || userUpdateRequest.getId() <= 0, ErrorCode.PARAMS_ERROR);
+        User user = new User();
+        BeanUtil.copyProperties(userUpdateRequest, user);
+        boolean res = userService.updateById(user);
+        ThrowUtils.ThrowIf(!res, ErrorCode.SERVER_ERROR, "更新用户失败");
+        return ResultUtils.success(true);
+    }
+
+    @PostMapping("/list/page/vo")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Page<UserVO>> listUserVOByPage(@RequestBody UserQueryRequest userQueryRequest) {
+        ThrowUtils.ThrowIf(userQueryRequest == null, ErrorCode.PARAMS_ERROR);
+        int current = userQueryRequest.getCurrentPage();
+        int pageSize = userQueryRequest.getPageSize();
+        ThrowUtils.ThrowIf(current <= 0 || pageSize <= 0, ErrorCode.PARAMS_ERROR);
+        Page<User> userPage = userService.page(new Page<>(current, pageSize), userService.getQueryWrapper(userQueryRequest));
+        Page<UserVO> userVOPage = new Page<>(current, pageSize,userPage.getTotal());
+        List<UserVO> userVOList = userService.getUserVOList(userPage.getRecords());
+        userVOPage.setRecords(userVOList);
+        return ResultUtils.success(userVOPage);
+    }
+
 }
