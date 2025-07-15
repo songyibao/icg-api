69c1f398fac5872ae75cdb6026e1720878763477
SongYibao <songybmail@foxmail.com>
2025-07-15 20:49:21 +0800

    使用redis存储session

--- src/main/java/com/yb/icgapi/IcgApiApplication.java
+++ src/main/java/com/yb/icgapi/IcgApiApplication.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @MapperScan("com.yb.icgapi.mapper")
 @EnableAspectJAutoProxy(exposeProxy = true)
+@EnableAsync
 public class IcgApiApplication {
 
     public static void main(String[] args) {
