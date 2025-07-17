20932bd75ca13af6e8a486a925564c8551805663
SongYibao <songybmail@foxmail.com>
2025-07-17 17:05:27 +0800

    调整注释

--- src/main/java/com/yb/icgapi/constant/PictureConstant.java
+++ src/main/java/com/yb/icgapi/constant/PictureConstant.java
@@ -6,7 +6,7 @@ public interface PictureConstant {
     // 图片简介限长
     int PICTURE_INTRODUCTION_MAX_LENGTH = 512;
     // 图片大小限制
-    long PICTURE_SIZE_MAX = 2 * 1024 * 1024; // 10MB
+    long PICTURE_SIZE_MAX = 2 * 1024 * 1024; // 2MB
     // 超过此大小的图片将生成缩略图
     long THUMBNAIL_SIZE_MAX = 100 * 1024; // 100KB
     // 缩略图最大宽度
